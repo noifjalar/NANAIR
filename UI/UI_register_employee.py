7831d7a4bd28e52cb863c90ac68fb7b83eede26e
noifjalar <noi_fjalar@hotmail.com>
2019-12-05 14:07:36 +0000

    df

---
+++ UI/UI_register_employee.py
@@ -0,0 +1,16 @@
+from Model.employee import Employee
+
+
+def register_employee_UI(self):
+    ssn = input("SSN: ")
+    name = input("Name: ")
+    #new_emp.name = name
+    address = input("Address: ")
+    role = input("Role: ")
+    rank = input("Rank: ")
+    licence = input("Licence: ")
+    phonenumber = input("Mobile: ")
+
+
+    
+    return emp=
