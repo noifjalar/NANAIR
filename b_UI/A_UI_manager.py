157a7eca3817e3c9538555058e27405a253a8e51
noifjalar <noi_fjalar@hotmail.com>
2019-12-05 16:02:56 +0000

    E

--- b_UI/A_UI_manager.py
+++ b_UI/A_UI_manager.py
@@ -1,7 +1,7 @@
 #from Logic.LogicManager import LogicManager
 #from Data.DataManager import DataManager
 
-from UI_employee import UI_employee
+from UI_Employee import Employee_UI
 from UI_Main_Menu import UI_Main_Menu
 
 class Manager_UI :
