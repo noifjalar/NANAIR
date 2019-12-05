4b41b2f0035fc55a02bd201e76590fbd230cca43
Rosa-Petursdottir <54938845+Rosa-Petursdottir@users.noreply.github.com>
2019-12-05 16:18:08 +0000

    l

--- b_UI/A_UI_manager.py
+++ b_UI/A_UI_manager.py
@@ -1,7 +1,7 @@
 #from Logic.LogicManager import LogicManager
 #from Data.DataManager import DataManager
 
-from b_UI.UI_Employee import Employee_UI
+from b_UI.UI_employee import Employee_UI
 from UI_Main_Menu import UI_Main_Menu
 
 class Manager_UI :
