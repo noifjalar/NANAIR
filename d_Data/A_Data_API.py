6a33e94069c5d15c1ef0440cda9e4d85e31808b9
Rosa-Petursdottir <54938845+Rosa-Petursdottir@users.noreply.github.com>
2019-12-06 14:48:27 +0000

    l

--- d_Data/A_Data_API.py
+++ d_Data/A_Data_API.py
@@ -2,16 +2,15 @@
 # its true
 # omg
 
-from Model import *
-from .Data_Employee import Employee_Data
+#from Model import *
+from d_Data.Data_Employee import Employee_Data
 
 
 class DataAPI : 
     def __init__(self) :
         self.demp = Employee_Data( )
-        pass
 
-    def storenewemplyee (self,  new_empl  ):
+    def storenewemplyee (self, new_empl ):
         return self.demp.register_employee_Data ( new_empl )
 
     def myprint(self):
