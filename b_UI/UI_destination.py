f285b1e1fdbbeeb8f46eaa30ab53c2112ba8474b
magnusthorm <mmagnusthor@gmail.com>
2019-12-07 01:53:24 +0000

    það er vi

--- b_UI/UI_destination.py
+++ b_UI/UI_destination.py
@@ -1,5 +1,5 @@
 from Model.destination import Destination
-from c_Logic.A_LL_API import A_LL_API
+from c_Logic.A_LL_API import LL_API
 from d_Data.Data_Destination import Destination_Data
 
 class UI_Destination:
@@ -9,5 +9,5 @@ class UI_Destination:
     def register_destination_UI(self):
         identity = input("ID: ")
         destination = input("Destination: ")
-        new_destination = Destination(identity, destination)
+        # new_destination = Destination(identity, destination, flight_time)
         self.la.addnewdestination(identity, destination, flight_time)
