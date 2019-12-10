a4335d2ace4e84c43c9e797e9aa858f1154dd54b
noifjalar <noi_fjalar@hotmail.com>
2019-12-10 23:25:02 +0000

    create voyage tilbúið, bara í UI samt :(

--- b_UI/UI_Voyage.py
+++ b_UI/UI_Voyage.py
@@ -1,6 +1,7 @@
 from Model.voyage import Voyage
 from c_Logic.A_LL_API import LL_API
 import datetime
+from datetime import timedelta
 
 
 class voyage_UI :
@@ -10,14 +11,9 @@ class voyage_UI :
 
     def register_voyage_UI(self):
         
-
         flight_number = input("Enter a flight number: ")
 
         departing_from = "KEF"
-
-
-        #voy_dest_time = ""
-        #arriving_at = ""
 
         dest_dict = self.la.voy_dest()
         counter = 1
@@ -28,7 +24,7 @@ class voyage_UI :
                 print("({}) - {}".format(counter, key))
                 counter += 1
         val = int(input("Choose a destination: "))
-        #print(val)
+        
         counter = 1
         for key, value in dest_dict.items():
             if key == "Keflavik": 
@@ -39,29 +35,30 @@ class voyage_UI :
                     arriving_at = value[0]
                     voy_dest_time = int(value[1])
                 counter += 1
-        #arriving_at = voy_destination
-        # print(voy_dest_time)
-        print(arriving_at)
-        input
+       
             
+        year, month, day = input("Input date of flight (YYYY MM DD): ").split()
+        hour, minute = input("Input time of flight (hour minute): ").split()
+
+        year = int(year)
+        month = int(month)
+        day = int(day)
+        hour = int(hour)
+        minute = int(minute)
         
-
-
-        year, month, day = int(input("Input date of flight (YYYY MM DD): ").split("."))
-        hour, minute = int(input("Input time of flight (hour.minute): ").split(":"))
-        print(year)
-        print(month)
-        print(day)
-        print(hour)
-        print(minute)
-        #hour= int(input('input a hour:'))
-        #minute= int(input('input a minute:'))
-
-
-        #year, month, day, hour, minute = year, month, day, hour, minute
-        departure = datetime.datetime(year, month, day, hour, minute).isoformat()
-        print(departure)
-        arrival = datetime.datetime(year, month, day, hour + voy_dest_time, minute).isoformat()
-
+        departure_not_iso = datetime.datetime(year, month, day, hour, minute)
+        arrival_not_iso = departure_not_iso + timedelta(hours= voy_dest_time)
+        departure = departure_not_iso.isoformat()
+        arrival = arrival_not_iso.isoformat()
+   
         self.la.addnewvoyage(flight_number, departing_from, arriving_at, departure, arrival)
 
+        flight_number_return = input("Enter a flight number for return flight: ")
+
+        departure_return = (arrival_not_iso + timedelta(hours= 1)).isoformat()
+        arrival_return = (arrival_not_iso + timedelta(hours= 1) + timedelta(hours= voy_dest_time)).isoformat()
+
+        departing_from_return = arriving_at
+        arriving_return = departing_from
+        
+        self.la.addnewvoyage(flight_number_return, departing_from_return, arriving_return, departure_return, arrival_return)
