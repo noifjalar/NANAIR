c186d28a31da61d37d61859c89a8f27f6874043a
Rosa-Petursdottir <54938845+Rosa-Petursdottir@users.noreply.github.com>
2019-12-13 00:55:58 +0000

    AIRCRAFT ID

--- c_Logic/LL_Aircraft.py
+++ c_Logic/LL_Aircraft.py
@@ -4,8 +4,8 @@ class Aircraft_LL :
     def __init__ (self):
         self.dapi = Aircraft_Data()
 
-    def addnewaircraft(self, nickname,planeTypeId,capacity,manufacturer):
-        self.dapi.register_aircraft_Data(nickname,planeTypeId,capacity,manufacturer)
+    def addnewaircraft(self, nickname, aircraftID, planeTypeId,capacity,manufacturer):
+        self.dapi.register_aircraft_Data(nickname,aircraftID,planeTypeId,capacity,manufacturer)
 
 
 
